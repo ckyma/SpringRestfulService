455cda903b23724fce6aa246a11577ec9a57b95a
Yu Chen <ckymacn@gmail.com>
2017-01-19 16:30:23 +0800

    First submission

---
+++ src/main/java/helllo/Application.java
@@ -0,0 +1,15 @@
+package main.java.helllo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by yuchen on 1/19/2017.
+ */
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/main/java/helllo/GreetingController.java
@@ -0,0 +1,24 @@
+package main.java.helllo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+/**
+ * Created by yuchen on 1/19/2017.
+ */
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                String.format(template, name));
+    }
+}
